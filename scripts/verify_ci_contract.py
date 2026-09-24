92a1154221256ba4e705328189da9db2c455f053
Ƴunior Ƥortal <35150859+portyu9@users.noreply.github.com>
2026-09-24 19:42:05 -0400

    security: rebind trusted-auto extension digest

--- scripts/verify_ci_contract.py
+++ scripts/verify_ci_contract.py
@@ -36,7 +36,7 @@ EXPECTED_WORKFLOW_NAMES = {
     "trusted-pr-auto.yml",
 }
 EXPECTED_TRUSTED_AUTO_EXTENSION_BLOB_SHA = (
-    "4d828e39ffaa129f3f825dead7dfb02f19f1651c"  # pragma: allowlist secret
+    "377ab205014e4f30b663a6d5699654108f1aebac"  # pragma: allowlist secret
 )
 EXPECTED_ORDINARY_CI_WORKFLOW_BLOB_SHA = (
     "7fdf0dc85375bc78561d531f95220cd877e30b3a"  # pragma: allowlist secret
