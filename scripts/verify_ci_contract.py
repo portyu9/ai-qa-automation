09c4df5dbc944ebe986515f75c8db663cbf91e3a
Ƴunior Ƥortal <35150859+portyu9@users.noreply.github.com>
2026-09-25 18:15:42 -0400

    ci: rebind trusted-auto contract blob

--- scripts/verify_ci_contract.py
+++ scripts/verify_ci_contract.py
@@ -36,7 +36,7 @@ EXPECTED_WORKFLOW_NAMES = {
     "trusted-pr-auto.yml",
 }
 EXPECTED_TRUSTED_AUTO_EXTENSION_BLOB_SHA = (
-    "44c6fbaddf9edbd778c85a1f0983d408e8650a4c"  # pragma: allowlist secret
+    "ac87e1d1d83e03bd5b8073aaade0addb880f181c"  # pragma: allowlist secret
 )
 EXPECTED_ORDINARY_CI_WORKFLOW_BLOB_SHA = (
     "7fdf0dc85375bc78561d531f95220cd877e30b3a"  # pragma: allowlist secret
